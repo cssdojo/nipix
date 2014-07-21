2112db33430d3168d53b23598d2dacc918bf1b07
Denis Pismenny <denis.pismenny@gmail.com>
2014-07-21 05:16:46 +0300

    done

--- js/common.js
+++ js/common.js
@@ -13,4 +13,48 @@ head.ready(function() {
 		});
 	} dropdown();
 
+	function modals() {
+		$(document).on("click", function(){
+			$(".js-overlay").hide();
+			$("body").removeClass("has-open-popup");
+		});
+		$(".js-popup-link").on("click", function(event){
+			$(".js-overlay").fadeOut(200);
+			var popup = $(this).attr("href");
+			$("body").addClass("has-open-popup");
+			$("."+popup).parent().fadeIn(200);
+			event.stopPropagation();
+			return false;
+		});
+		$(".js-popup-close").on("click", function(){
+			$(".js-overlay").fadeOut(200);
+			$("body").removeClass("has-open-popup")
+			return false;
+		});
+		$(".js-popup").on("click", function(event){
+			event.stopPropagation();
+		});
+	} modals();
+
+	$('.js-slider').each(function(){
+		var p = this.parentNode;
+		$(this).cycle({
+			fx: "carousel",
+			// autoHeight: "container",
+			timeout:  0,
+			carouselVisible: 1,
+			slideActiveClass: "is-active",
+			pagerActiveClass: "is-active",
+			disabledClass: "is-disabled",
+			slideClass: "slider__slide",
+			allowWrap: true,
+			carouselFluid: "true",
+			slides: $('.slider__slide', p),
+			prev:   $('.slider__prev', p),
+			next:   $('.slider__next', p),
+			pager: ".slider__pager",
+			pagerTemplate: "<span>•</span>"
+		});
+	});
+
 });
